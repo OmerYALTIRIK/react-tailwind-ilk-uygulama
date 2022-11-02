8deae3637555407f8d52e7ec63c6c292b3f0d53c
Ömer Yaltırık <40443652+OmerYALTIRIK@users.noreply.github.com>
2022-11-02 03:58:27 +0300

    Tailwind öğrenme serisi-3. commit

---
+++ src/Components/Merhaba.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Merhaba() {
+  return (
+    <div className='shadow-md mt-7'>Merhaba</div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    colors: {
+      'sosyalBilisim': '#1fb6ff',
+      'mehmet': '#354678',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+    },
   },
   plugins: [],
 }
